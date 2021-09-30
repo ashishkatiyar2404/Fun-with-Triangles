784e447eb395983f0a1980e8a903f2b59ebfaaa9
Ashish Katiyar <katiyarashish24@gmail.com>
2021-09-30 16:30:53 +0530

    file added

---
+++ area.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll('.area-input');
+const areaBtn = document.querySelector('#area-btn');
+const areaOutput = document.querySelector('#area-output');
+function multiplyBaseAndHeight(base, height) {
+	const multipliedOutput = base * height;
+    //console.log(multipliedOutput);
+	return multipliedOutput;
+}
+
+function calculateArea() {
+	//area = (base * height)/2
+	const basexheight = multiplyBaseAndHeight(inputs[0].value, inputs[1].value);
+	const area = basexheight / 2;
+	//console.log({ area });
+	areaOutput.innerText = `The area of the triangle is ${area} cm²`;
+}
+
+areaBtn.addEventListener('click', calculateArea);
--- hypo.js
+++ hypo.js
@@ -3,15 +3,16 @@ const hypotenuseBtn = document.querySelector('#hypotenuse-btn');
 const hypotenuseOutput = document.querySelector('#hypotenuse-output');
 function calculateSumOfSquares(a, b) {
 	const sumOfSquares = a * a + b * b;
+    //console.log(sumOfSquares);
 	return sumOfSquares;
 }
 
 function calculateHypotenuse() {
 	//c = sqrt(a^2 + b^2)
 	const sumOfSquares = calculateSumOfSquares(sides[0].value, sides[1].value);
-	console.log(sumOfSquares);
+	//console.log(sumOfSquares);
 	const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
-	console.log(lengthOfHypotenuse);
+	//console.log(lengthOfHypotenuse);
 	hypotenuseOutput.innerText = `The length of the hypotenuse is ${lengthOfHypotenuse} cm`;
 }
 
